78cacfdbeb827e2636cfc4b9263567400ff7741d
CerberusProgrammer <66286300+CerberusProgrammer@users.noreply.github.com>
2024-04-12 11:46:11 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="p-8 text-3xl">hola</p>
+    </>
+  );
+}
+
+export default App;
